464e18ecd83906ebf2d346e7acc2bc0544aa1e6f
luisfe2127 <luis.canarinho2127@gmail.com>
2023-11-27 12:56:57 -0300

    atualizando object

--- aula 16/object05.js
+++ aula 16/object05.js
@@ -1,5 +1,37 @@
 let message = "Hello world!";
 let x = message.toUpperCase();
 
-console.log(x)
+console.log(x);
+
+/*const person = {
+    fName: "John",
+    lName: "Con",
+    fullName: function() {return this.fName + " " + this.lName}
+}
+
+console.log(person.fullName)*/
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    get fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+  console.log(person.fullName)
+
+  const perso = {
+    id: 5670
+  }
+
+  console.log(perso.id)
+
+  perso.id = 2178
+
+  console.log(perso.id)
+
+
+
+
   
---
+++ aula 16/object06.js
@@ -0,0 +1,16 @@
+const o = {
+    id: 3450,
+    f: function() {return this.id}
+}
+
+console.log(o.f())
+
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+  }
+  
+  const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
