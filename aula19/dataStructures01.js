ae0e369edc2a68fbe08f9ecec50f088ed3e0b5ce
luisfe2127 <luis.canarinho2127@gmail.com>
2023-12-08 11:20:57 -0300

    atualizando dataStructures

---
+++ aula19/dataStructures01.js
@@ -0,0 +1,27 @@
+const arr = [2.5, 5, 10, 30, 40, 50]
+
+console.log(arr[3])
+
+console.log(arr.length)
+
+
+
+
+const obj = {
+    p1: "Pedro",
+    p2: function() {console.log("My name is")},
+}
+
+console.log(obj.p1)
+obj.p2()
+
+
+
+
+obj.p3 = 145
+obj["p4"] = "GOW"
+obj.p5 = () => console.log("TLOU")
+
+console.log(obj.p3)
+console.log(obj["p4"])
+obj.p5()
