a097ae6d13f21800ef598ef17a4884043bd86f22
luisfe2127 <luis.canarinho2127@gmail.com>
2024-01-09 16:39:27 -0300

    atualizando dataStructures

---
+++ aula19/dataStruuctures04.js
@@ -0,0 +1,41 @@
+//push = adiciona +1
+//pop = remove o ultimo elemento 
+//shift = remove o primeiro elemento
+//unshift = adiciona um elemento para o primeiro elemento dentro de uma array ou variavel constante 
+//
+
+
+
+
+const filaDoBanco = []
+
+ filaDoBanco.push("Jorge","Sorza","Vitor","Celso","Lucas","Boinha")
+
+ console.log(filaDoBanco)
+
+ filaDoBanco.pop()
+ filaDoBanco.pop()
+
+ console.log(filaDoBanco)
+
+ filaDoBanco.unshift("jao")
+
+ console.log(filaDoBanco)
+
+ filaDoBanco.shift()
+ filaDoBanco.shift()
+
+ console.log(filaDoBanco)
+
+ let _v = 0;
+
+ let obj = {
+    get v() {
+        return "Value:" + _v;
+    },
+    set v(value) {
+        _v = value * 2;
+    }
+ }
+
+ console.log(obj)
