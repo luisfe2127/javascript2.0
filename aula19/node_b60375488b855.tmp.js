ee07740d027183fb90a6f8fe8a295a78afeb534d
Luís Felipe <luis.canarinho2126@gmail.com>
2024-01-23 13:37:04 -0300

    atualizando dataStructure

---
+++ aula19/node_b60375488b855.tmp.js
@@ -0,0 +1,39 @@
+// We create a class for each node within the list
+class Node{
+    // Each node has two properties, its value and a pointer that indicates the node that follows
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+// We create a class for the list
+class SinglyLinkedList{
+    // The list has three properties, the head, the tail and the list size
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+    // The push method takes a value as parameter and assigns it as the tail of the list
+    push (val) {
+        const newNode = new Node(val)
+
+        if(!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+}
+
+const myList = new SinglyLinkedList();
+
+myList.push(10).push(20).push(30);
+
+console.log(myList);
