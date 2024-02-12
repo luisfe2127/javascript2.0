79bdfa199a46f926865bfa35ce84d5d956de96b1
Luís Felipe <luis.canarinho2126@gmail.com>
2024-02-12 10:56:44 -0300

    atualizando dataStructures

--- aula19/dataStucture06.js
+++ aula19/dataStucture06.js
@@ -52,4 +52,72 @@ class DoublyLinkedList {
         this.length--
         return poppedNode
     }
+
+    shift() {
+        if(!this.head) return undefined;
+
+        let oldHead = this.head
+
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+
+            this.head = oldHead.next
+            this.head.prev = null
+            oldHead.next = null
+        }
+
+        this.length--
+        return oldHead
+    }
+
+    unshift(val) {
+        let newNode = new Node(val)
+
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+
+        this.length++
+        return this
+    }
+
+    get(index) {
+
+        if (index < 0 || index >= this.length) return undefined;
+
+        let current, count
+
+        if (index <= this.length/2) {
+
+            current = this.head
+            count = 0
+
+            while(count !== index) {
+                current = current.next
+                count++
+            }
+
+        } else {
+
+            current = this.tail
+            count = this.tail - 1
+
+            while(count !== index) {
+                current = current.prev
+                count--
+            }
+        }
+
+        return current
+
+
+    }
+
 }
