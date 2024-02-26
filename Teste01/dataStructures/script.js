73de1dc4d83cf89caae0d2ffdf49ff49910a4617
Luís Felipe <luis.canarinho2126@gmail.com>
2024-02-26 16:53:28 -0300

    atualizando dataStructures

---
+++ Teste01/dataStructures/script.js
@@ -0,0 +1,50 @@
+let valor = document.getElementById("ivalor");
+let res = document.getElementById("res");
+let valores = document.getElementById("ivalores");
+
+ document.getElementById("enviar").onclick = function() {
+
+class Node {
+
+    constructor(valor) {
+
+        this.valor = valor
+        this.next = null
+    }
+}
+    
+    class listLinkedImplementation {
+
+    constructor() {
+
+        this.head = head
+        this.tail = tail
+        this.length = length
+
+    }
+
+push(valor) {
+
+    const newNode = new Node(valor)
+
+    if(this.head === null) {
+
+        this.head = newNode
+        this.tail = this.head
+
+    } else {
+
+    this.tail.next = newNode
+    this.tail = newNode
+
+    }
+
+    this.length++
+    return this 
+}
+
+ document.getElementById("ivalores").innerHTML = return this
+
+ }
+ }
+
