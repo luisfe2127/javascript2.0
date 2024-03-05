865a319579acd8b854745f7b22d458ec5ba3eb5d
Luís Felipe <luis.canarinho2126@gmail.com>
2024-03-05 14:03:04 -0300

    atualizando dataStructures

--- Teste01/dataStructures/dataStructuresTest02/matrixScript.js
+++ Teste01/dataStructures/dataStructuresTest02/matrixScript.js
@@ -5,8 +5,10 @@ class Graph {
         this.adjacencyList = {}
     }
 
-    addNode(node) {
-        if(!this.adjacencyList) this.adjacencyList[node] = []
+    addNode(value) {
+        if (!this.adjacencyList[value]) {
+            this.adjacencyList[value] = [];
+        }
     }
 
     addConection(node1, node2) {
@@ -15,15 +17,15 @@ class Graph {
 
         this.adjacencyList[node1].push(node2)
         this.adjacencyList[node2].push(node1)
-
     }
 
-    removeConection(node1, node2) {
+    removeConnection(node1, node2) {
         if(!this.adjacencyList[node1] || !this.adjacencyList[node2]) return
 
         this.adjacencyList[node1] = this.adjacencyList[node1].filter(v = v !== node2)
 
         this.adjacencyList[node2] = this.adjacencyList[node2].filter(v = v !== node1)
+
     }
 
     removeNode(node) {
@@ -33,32 +35,58 @@ class Graph {
             let adjacentNode = this.adjacencyList[node].push
             if(adjacentNode == null) break;
 
-            this.removeConection[node, adjacentNode]
+            this.removeConnection[node, adjacentNode]
         }
 
         delete this.adjacencyList[node]
+
+    }
+  
+}
+
+let matrix = new Graph()
+
+function updateDisplay() {
+    const displayList = document.getElementById("displayList");
+    displayList.innerHTML = ""; 
+
+    for (let value in matrix.adjacencyList) {
+        displayList.innerHTML += value + ": " + matrix.adjacencyList[value].join(", ") + "<br>";
     }
 
+}
 
 
-    displayList() {
-        let listDisplay = document.getElementById('listDisplay');
-        let current = this.head;
-        let listContent = '';
-        
-        while (current !== null) {
-            listContent += current.value + ' -> ';
-            current = current.next;
-        }
-        
-        listDisplay.textContent = listContent;
-    }
+function submitAddVal() {
+    const value = document.getElementById("addVal").value;
+    matrix.addNode(value);
+
+    updateDisplay();
+}
+
+function addConection() {
+
+    let valDad = document.getElementById("addConectionDad").value
+    let valNewSon = document.getElementById("addConectionSon").value
+
+    matrix.insert(valDad, valNewSon)
+}
+
+function removeConnection() {
+
+    let valDad = document.getElementById("unlinkConectionDad").value
+    let valSon = document.getElementById("unlinkConectionSon").value
+
+    matrix.remove(valDad, valSon)
+    
+}
+
+function removeNode() {
+
+    let valDad = document.getElementById("unlinkVal").value
+
+    matrix.remove(valDad)
 }
 
 
 
-
-
-
-}
-}
