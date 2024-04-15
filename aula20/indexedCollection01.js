9507680f03de0f69679a149b1a770e53f5dd352e
Luís Felipe <luis.canarinho2126@gmail.com>
2024-04-15 12:03:06 -0300

    atualizando indexCollection01

--- aula20/indexedCollection01.js
+++ aula20/indexedCollection01.js
@@ -58,3 +58,11 @@ produtos.forEach((produtoAtual) => {
     console.log(produtoAtual)
 })
 
+const items = [12, 24, 36];
+const copy = [];
+items.forEach(function (item) {
+    copy.push(item + item + 2);
+});
+console.log(copy);
+  
+ 
