a71872c923e4a327541b63ee1fbc814b78de2e70
Luís Felipe <luis.canarinho2126@gmail.com>
2024-06-27 11:23:23 -0300

    atualizando conditionalStatements01

---
+++ aula31/conditionalStatements01.js
@@ -0,0 +1,10 @@
+let calc = function mult(number) {
+    return number * 2
+}
+
+let x = calc(4)
+console.log(x)
+
+let iorp = function imparOrPar(number) {
+    return number % 1 
+}
---
+++ aula31/node_bc3e022ee8307.tmp.js
@@ -0,0 +1,7 @@
+let calc = function mult(number) {
+for (number = 0; number < 10; number *= 2) {
+ 
+}
+}
+
+console.log(calc(2))
