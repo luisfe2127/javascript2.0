0a9628c5943b0de61ff7787c1bc765ac9e63f3aa
Luís Felipe <luis.canarinho2126@gmail.com>
2024-07-11 10:59:05 -0300

    atualizando callFunctions

--- aula32/callFunctions.js
+++ aula32/callFunctions.js
@@ -1,2 +1,12 @@
 //Funções aninhadas 
 
+function soma(...valores) {
+    function somar() {
+        let compriment = valores.length
+        let res = 0
+        for(i = 0; i < compriment; i++) {
+            res += compriment[i]
+        }
+    }
+}
+
